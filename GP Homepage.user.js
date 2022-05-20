9422a83bd247c62caf3a1bb5218584795af6d39c
AdamDnd <69721786+AdamDnd@users.noreply.github.com>
2022-05-20 07:57:38 +0100

    Update GP Homepage.user.js

--- GP Homepage.user.js
+++ GP Homepage.user.js
@@ -1,7 +1,9 @@
 // ==UserScript==
 // @name         GP Homepage
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
+// @updateURL    https://github.com/AdamDnd/GamersPlaneScripts/raw/main/GP%20Homepage.user.js
+// @downloadURL  https://github.com/AdamDnd/GamersPlaneScripts/raw/main/GP%20Homepage.user.js
 // @description  Format bookmarked games section
 // @author       Adam
 // @match        https://gamersplane.com
@@ -17,12 +19,61 @@
 
     GM_addStyle('.hilightGame{background-color:#ffffdd;} .hilightGame .td.lastPost a.username{font-weight:bold;} #yourGames .td.name a {white-space: nowrap;}');
     GM_addStyle('body.dark .hilightGame{background-color:#3e0700;}');
+    GM_addStyle('.congratsList{margin: 1px 20px 0.5rem 20px;background-color: #eee;padding: 1rem;color: #000;}');
+    GM_addStyle('body.dark .congratsList{color: #fff;background-color: #37393f;}');
 
     $.ajax({type: 'post',url: API_HOST +'/users/getCurrentUser',xhrFields: {withCredentials: true},data:{},
             success:function (data) {
                 if(data && data.userID){
                     //Highlight bookmarked games where I wasn't the last to post
                     $('#yourGames:eq(0) .tr:not(.favGame):not(:has(.td.lastPost a.username[href="/user/'+data.userID+'/"]))').addClass('hilightGame');
+                    var congratulations=[];
+                    var dtJoined=new Date(data.joinDate);
+                    var dtNowUtc=new Date();
+                    if(dtJoined.getDate()==dtNowUtc.getUTCDate() && dtJoined.getMonth()==dtNowUtc.getUTCMonth()){
+                        var yDiff=(dtNowUtc.getUTCFullYear()-dtJoined.getFullYear());
+                        if(yDiff>0){
+                            congratulations.push('🎂 You joined '+yDiff+' year'+(yDiff>1?'s':'')+' ago.  Happy GP anniversary!');
+                       }
+                    }
+                    $.ajax({type: 'post',url: API_HOST +'/users/stats',xhrFields: {withCredentials: true},data:{userID:data.userID},
+                            success:function (data) {
+                                var checkForSpecialNumbers=function(val,valText){
+                                    if(val){
+                                        if(val%10000==0){
+                                            congratulations.push("💎 You've made "+val+' '+valText+'!');
+                                        }
+                                        else if(val%5000==0){
+                                            congratulations.push("🥇 You've made "+val+' '+valText+'!');
+                                        }
+                                        else if(val%1000==0){
+                                            congratulations.push("🏆 You've made "+val+' '+valText+'!');
+                                        }
+                                        else if(val%500==0){
+                                            congratulations.push("😎 You've made "+val+' '+valText+'!');
+                                        }
+                                        else if(val%100==0){
+                                            congratulations.push("⭐ You've made "+val+' '+valText+'!');
+                                        }
+                                    }
+                                };
+                                if(data.posts){
+                                    checkForSpecialNumbers(data.posts.communityCount,'community posts');
+                                    checkForSpecialNumbers(data.posts.gameCount,'game posts');
+                                    if(data.posts.communityCount>0 && data.posts.gameCount>0){
+                                        checkForSpecialNumbers(data.posts.count,'posts');
+                                    }
+                                }
+                            },
+                            complete:function(data){
+                                if(congratulations.length>0){
+                                    var congrats=$('.congratsList',$('<div class="col-1"><h2 class="headerbar notificationsheaderbar"><i class="ra ra-diamond"></i> Congratulations</h2><div class="congratsList"></div></div>').prependTo($('#page_home')));
+                                    for(var i=0;i<congratulations.length;i++){
+                                        $('<h3></h3>').text(congratulations[i]).appendTo(congrats);
+                                    }
+                                }
+                            }
+                           });
                 }
             }
         });
