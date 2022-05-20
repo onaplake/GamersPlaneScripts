e3a80ba27f61d3f69eca4d07908e9aaf2baf127f
AdamDnd <69721786+AdamDnd@users.noreply.github.com>
2022-05-20 05:49:18 +0100

    Update GP Homepage.user.js

--- GP Homepage.user.js
+++ GP Homepage.user.js
@@ -1,29 +1,31 @@
 // ==UserScript==
 // @name         GP Homepage
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @updateURL    https://github.com/AdamDnd/GamersPlaneScripts/raw/main/GP%20Homepage.user.js
-// @downloadURL  https://github.com/AdamDnd/GamersPlaneScripts/raw/main/GP%20Homepage.user.js
+// @version      0.2
 // @description  Format bookmarked games section
 // @author       Adam
 // @match        https://gamersplane.com
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=gamersplane.com
 // @grant        GM_addStyle
 // ==/UserScript==
-/* globals jQuery, $, moment */
+/* globals jQuery, $, moment, API_HOST */
 
 (function() {
     'use strict';
-
-    var myUserId=5988;
 
     moment.lang('en', {calendar: {lastDay: '[Yesterday ] HH:mm', sameDay: '[Today ] HH:mm', nextDay: '[Tomorrow ] HH:mm', lastWeek: '[last] dddd HH:mm', nextWeek: 'dddd [at] HH:mm', sameElse: 'D MMM HH:mm'}});
 
     GM_addStyle('.hilightGame{background-color:#ffffdd;} .hilightGame .td.lastPost a.username{font-weight:bold;} #yourGames .td.name a {white-space: nowrap;}');
     GM_addStyle('body.dark .hilightGame{background-color:#3e0700;}');
 
-    //Highlight bookmarked games where I wasn't the last to post
-    $('#yourGames:eq(0) .tr:not(.favGame):not(:has(.td.lastPost a.username[href="/user/'+myUserId+'/"]))').addClass('hilightGame');
+    $.ajax({type: 'post',url: API_HOST +'/users/getCurrentUser',xhrFields: {withCredentials: true},data:{},
+            success:function (data) {
+                if(data && data.userID){
+                    //Highlight bookmarked games where I wasn't the last to post
+                    $('#yourGames:eq(0) .tr:not(.favGame):not(:has(.td.lastPost a.username[href="/user/'+data.userID+'/"]))').addClass('hilightGame');
+                }
+            }
+        });
 
     //change last posting date to human readable
     $('.convertTZ').each(function(){
