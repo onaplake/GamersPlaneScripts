9ef05a926d5afb99720de5737009de684fc8ec82
AdamDnd <69721786+AdamDnd@users.noreply.github.com>
2022-06-15 07:31:56 +0100

    Update GP Homepage.user.js

--- GP Homepage.user.js
+++ GP Homepage.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GP Homepage
 // @namespace    http://tampermonkey.net/
-// @version      0.31
+// @version      0.32
 // @updateURL    https://github.com/AdamDnd/GamersPlaneScripts/raw/main/GP%20Homepage.user.js
 // @downloadURL  https://github.com/AdamDnd/GamersPlaneScripts/raw/main/GP%20Homepage.user.js
 // @description  Format bookmarked games section
@@ -17,16 +17,39 @@
 
     moment.lang('en', {calendar: {lastDay: '[Yesterday ] HH:mm', sameDay: '[Today ] HH:mm', nextDay: '[Tomorrow ] HH:mm', lastWeek: '[last] dddd HH:mm', nextWeek: 'dddd [at] HH:mm', sameElse: 'D MMM HH:mm'}});
 
-    GM_addStyle('.hilightGame{background-color:#ffffdd;} .hilightGame .td.lastPost a.username{font-weight:bold;} #yourGames .td.name a {white-space: nowrap;}');
-    GM_addStyle('body.dark .hilightGame{background-color:#3e0700;}');
+    GM_addStyle('.hilightGame:not(.snoozed){background-color:#ffffdd;} .hilightGame:not(.snoozed) .td.lastPost a.username{font-weight:bold;} #yourGames .td.name {white-space: nowrap;}');
+    GM_addStyle('body.dark .hilightGame:not(.snoozed){background-color:#3e0700;}');
     GM_addStyle('.congratsList{margin: 1px 20px 0.5rem 20px;background-color: #eee;padding: 1rem;color: #000;}');
     GM_addStyle('body.dark .congratsList{color: #fff;background-color: #37393f;}');
+    GM_addStyle('#yourGames .ra-hourglass{color:#888;cursor:pointer;} #yourGames .snoozed .ra-hourglass{color:#333;} body.dark #yourGames .snoozed .ra-hourglass{color:#c54;}');
+
+    $('#yourGames:eq(0) .tr .td.name').on('click','.ra-hourglass',function(){
+        var pThis=$(this);
+        var dt=pThis.data('snoozedt');
+        var key=pThis.data('snoozekey');
+        pThis.closest('.tr').toggleClass('snoozed');
+        localStorage.setItem(key,dt);
+    });
 
     $.ajax({type: 'post',url: API_HOST +'/users/getCurrentUser',xhrFields: {withCredentials: true},data:{},
             success:function (data) {
                 if(data && data.userID){
                     //Highlight bookmarked games where I wasn't the last to post
                     $('#yourGames:eq(0) .tr:not(.favGame):not(:has(.td.lastPost a.username[href="/user/'+data.userID+'/"]))').addClass('hilightGame');
+
+                    //snoozing
+                    $('#yourGames:eq(0) .tr.hilightGame:has(.td.name a):not(.favGame)').each(function(){
+                        var pThis=$(this);
+                        var dt=moment($('.td.lastPost .convertTZ',pThis).text(),'MMM D, YYYY h:mm a').toDate().getTime();
+                        var fname=$('.td.name a',pThis)
+                        var key='snooze-'+$('.td.name a',pThis).attr('href');
+                        $('<i class="ra ra-hourglass"></i>').prependTo($('.td.name',pThis)).data('snoozedt',dt).data('snoozekey',key);
+                        console.log(dt);
+                        if(localStorage.getItem(key)==dt){
+                            pThis.addClass('snoozed');
+                        }
+                    });
+
                     var congratulations=[];
                     var dtJoined=new Date(data.joinDate);
                     var dtNowUtc=new Date();
