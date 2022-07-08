1039a3e300cf1ce028d21e6a34f160a6cf6782f7
AdamDnd <69721786+AdamDnd@users.noreply.github.com>
2022-07-08 18:06:32 +0100

    Update Game themes.user.js
    Creepy style

--- Game themes.user.js
+++ Game themes.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Game themes
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.12
 // @updateURL    https://github.com/AdamDnd/GamersPlaneScripts/raw/main/Game%20themes.user.js
 // @downloadURL  https://github.com/AdamDnd/GamersPlaneScripts/raw/main/Game%20themes.user.js
 // @description  Add styling to character sheets
@@ -302,6 +302,187 @@ a.fancyButton:visited,
     color:#02F9FF;
     border-radius:0;
     border-color:#01959F;
+}
+
+`,
+// creepy style
+creepy:`
+@import url('https://fonts.googleapis.com/css2?family=Jolly+Lodger&display=swap');
+
+
+.headerbar:after{
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 4px;
+    top: auto;
+    background-color:#800 !important;
+    filter: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg"><filter id="gpRandomnessSvg"><feTurbulence x="0" y="0" baseFrequency="0.006" type="fractalNoise" numOctaves="5"></feTurbulence><feDisplacementMap in="SourceGraphic" scale="20" /></filter></svg>#gpRandomnessSvg');
+}
+
+.headerbar:before{
+    display:none;
+}
+
+.fancyButton:before,
+.fancyButton:after,
+.trapezoid.redTrapezoid:before,
+.trapezoid.redTrapezoid:after
+{
+    left: 0;
+    width: 100%;
+    height: 4px;
+}
+
+.fancyButton:before,
+.trapezoid.redTrapezoid:before{
+    top: 0;
+}
+
+.fancyButton:after,
+.trapezoid.redTrapezoid:after{
+    bottom: 0;
+    top:auto;
+}
+
+
+button.fancyButton,
+button.fancyButton:active,
+button.fancyButton:visited,
+a.fancyButton,
+a.fancyButton:active,
+a.fancyButton:visited,
+.trapezoid.redTrapezoid{
+    margin:0 10px;
+    font-family:'Jolly Lodger',serif;
+    font-variant: small-caps;
+    padding:4px 10px;
+    background-color: transparent;
+    color: #a71300;
+    border-left: solid 1px;
+    border-right: solid 1px;
+}
+
+h1.headerbar, h2.headerbar, h3.headerbar, h4.headerbar{
+    background-color:transparent;
+    color:#333;
+    font-family:'Jolly Lodger',serif;
+}
+
+.breadcrumbs,
+#breadcrumbs{
+    font-family: 'Jolly Lodger',serif;
+    font-size:120%;
+}
+
+#page_forum_thread .postHeader {
+    border-width:3px;
+    border-radius: 0 0 500% 0/0 0 50% 0;
+}
+
+#page_forum_thread .postHeader .subject{
+    font-family: 'Jolly Lodger',serif;
+    font-weight:normal;
+    font-size:130%;
+}
+
+#page_forum_thread .postContent {
+    -webkit-border-radius: 2px;
+    -moz-border-radius: 2x;
+    border-radius: 2px;
+    box-shadow: 0 0 5px rgb(0 0 0 / 20%), 0 0 2rem #D3C3AD inset;
+    background: #f8f8f8;
+    border-width:0;
+}
+
+#page_forum_thread .postBlock{
+    position:relative;
+}
+
+
+.postBlock:nth-child(3):after,
+.postBlock:nth-child(13):after,
+.postBlock:nth-child(19):after{
+    position:absolute;
+    content:' ';
+    right: 0;
+    left:0;
+    height:40px;
+    width:100%;
+    top:0;
+}
+
+.postBlock:nth-child(3):after{
+    filter: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg"><filter id="gpRandomnessSvg"><feTurbulence x="0" y="0" baseFrequency="0.17" type="fractalNoise" numOctaves="5"></feTurbulence><feDisplacementMap in="SourceGraphic" scale="20" /></filter></svg>#gpRandomnessSvg');
+    background: radial-gradient(circle at top center, #000 0, transparent 40px);
+}
+
+.postBlock:nth-child(13):after{
+    filter: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg"><filter id="gpRandomnessSvg"><feTurbulence x="0" y="0" baseFrequency="0.13" type="fractalNoise" numOctaves="5"></feTurbulence><feDisplacementMap in="SourceGraphic" scale="20" /></filter></svg>#gpRandomnessSvg');
+    background: radial-gradient(circle at top right, #880 0, transparent 50px);
+}
+
+.postBlock:nth-child(19):after{
+    filter: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg"><filter id="gpRandomnessSvg"><feTurbulence x="0" y="0" baseFrequency="0.16" type="fractalNoise" numOctaves="5"></feTurbulence><feDisplacementMap in="SourceGraphic" scale="20" /></filter></svg>#gpRandomnessSvg');
+    background: radial-gradient(circle at top center, #800 0, transparent 80px);
+}
+
+
+.postNames{
+    font-family: 'Jolly Lodger',serif;
+    font-size:130%;
+}
+
+#page_forum_thread .posterDetails .charName, #page_forum_thread .posterDetails .posterName
+{
+    font-weight:normal;
+}
+
+
+#page_forum_thread .postPoint,
+#page_forum_thread .postBlock .postContent:after,
+#page_forum_thread .postBlock .postContent:before{
+    display:none;
+}
+
+#charSheetRoller h3.charName{
+    font-family: 'Jolly Lodger',serif;
+    font-variant: small-caps;
+}
+
+div.headerbar{
+background-color:transparent;
+    color:inherit;
+}
+
+
+body.dark h1.headerbar,body.dark h2.headerbar, body.dark h3.headerbar, body.dark h4.headerbar{
+    color:#D3C3AD;
+}
+
+body.dark #page_forum_thread .postContent {
+    box-shadow: 0 0 5px rgb(0 0 0 / 20%), 0 0 2rem #51493E inset;
+}
+
+body.dark button.fancyButton,
+body.dark a.fancyButton,
+body.dark a.fancyButton:active,
+body.dark a.fancyButton:visited,
+body.dark .trapezoid.redTrapezoid{
+    color: #c54;
+}
+
+body.dark button.fancyButton:after,
+body.dark button.fancyButton:before{
+    background-color: #c54;
+}
+
+.ra-quill-ink:before {
+    content: "\\e927";
+}
+
+.ra.forum-root-2:before{
+    content: "\\eaa2";
 }
 
 `
